84175a5a234fc7b0df07597fa06fcf7976248c86
Ashvin Moro <amoro@vmware.com>
2018-11-28 16:07:22 -0800

    Added lombok support

--- src/main/java/com/vmware/borathon/intertwine/model/Entity.java
+++ src/main/java/com/vmware/borathon/intertwine/model/Entity.java
@@ -1,7 +1,14 @@
 package com.vmware.borathon.intertwine.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Entity {
     private Type type;
     private String version;
